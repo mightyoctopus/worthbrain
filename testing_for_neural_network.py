f713a5e90fc35303db193fd6191e2ac644df632c
mhhong <mh.hong@proton.me>
2026-01-05 00:39:30 +0900

    Add neutal network model files(pt, joblib)

--- testing_for_neural_network.py
+++ testing_for_neural_network.py
@@ -1,5 +1,6 @@
 import math
 import matplotlib.pyplot as plt
+from typing import Dict
 
 from testing import Tester
 
@@ -12,22 +13,22 @@ COLOR_MAP = {"red": RED, "orange": YELLOW, "green": GREEN}
 
 class TesterForNeuralNetwork(Tester):
 
-    def __init__(self, predictor, data, prices, titles, size=250):
-        super().__init__(predictor, data, size)
-        self.prices = prices
-        self.titles = titles
+    def __init__(self, predictor, data, title=None, size=250):
+        super().__init__(predictor, data, title=None, size=size)
+        self.data = data
 
     def run_datapoint(self, i):
         datapoint = self.data[i]
-        guess = self.predictor(datapoint)
-        truth = self.prices[i]
+        guess = self.predictor(datapoint["item"])
+        truth = datapoint["price"]
+        title = datapoint["title"]
         
         error = abs(guess - truth)
         log_error = math.log(truth + 1) - math.log(guess + 1)
         sle = log_error ** 2
         color = self.color_for(error, truth)
         
-        title = self.titles[i] if len(self.titles[i]) <= 40 else self.titles[i][:40] + "..."
+        short_title = title[:40] + "..." if len(title) > 40 else title
         
         self.guesses.append(guess)
         self.truths.append(truth)
@@ -36,8 +37,4 @@ class TesterForNeuralNetwork(Tester):
         self.colors.append(color)
         
         print(
-            f"{COLOR_MAP[color]}{i + 1}: Guess: ${guess:,.2f} Truth: ${truth:,.2f} Error: ${error:,.2f} SLE: {sle:,.2f} Item: {title}{RESET}")
-
-    @classmethod 
-    def test(cls, function, data, prices, titles):
-        cls(function, data, prices, titles).run()
+            f"{COLOR_MAP[color]}{i + 1}: Guess: ${guess:,.2f} Truth: ${truth:,.2f} Error: ${error:,.2f} SLE: {sle:,.2f} Item: {short_title}{RESET}")
