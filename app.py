8e2fcf2d54023a1870c450ec0704d2229ba8486d
mhhong <mh.hong@proton.me>
2026-02-01 18:08:40 +0900

    Build the scaffold

--- app.py
+++ app.py
@@ -10,3 +10,18 @@ from deal_agent_framework import DealAgentFramework
 from log_utils import reformat
 
 load_dotenv(override=True)
+
+
+class App:
+    def __init__(self):
+        ### lazy initialization
+        self.agent_framework = None
+    ### And assign it here
+    def get_agent_framework(self):
+        if not self.agent_framework:
+            self.agent_framework = DealAgentFramework()
+
+        return self.agent_framework
+
+    def run(self):
+        pass
